31c6e18553e376bc4ddc23ad73a7ae1a452cea15
Conduit <sekonyelasentle081@gmail.com>
2026-08-29 20:01:29 +0200

    Delete src directory

--- src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Sidebar from '@/components/Sidebar'
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex">
-      <Sidebar role="admin" />
-      <main className="flex-1 p-8">{children}</main>
-    </div>
-  )
-}
--- src/lib/supabase/server.ts
+++
@@ -1,3 +0,0 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-export const createClient = () => createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, { cookies: { get: (n) => cookies().get(n)?.value }})
